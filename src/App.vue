a1b3b59e16fbad548b4e5f000c652dbdb6c6fe4c
Hüseyin Ufuk Yılmaz <ufukyilmaz0706@hotmail.com>
2024-07-25 03:39:32 +0300

    Commit

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,54 @@
 <template>
     <div class="weather-app">
         <header class="header">
-            <h1>Canlı Hava Durumu</h1>
+            <h1>{{this.languageEn.header}}</h1>
         </header>
         <div class="selection-container">
             <form @submit.prevent="fetchWeatherData" class="weather-form">
                 <div class="form-group">
-                    <label for="country">Ülke:</label>
+                    <label for="country">{{languageEn.weather.country}}:</label>
                     <select id="country" v-model="selectedCountry" @change="fetchCities">
-                        <option v-for="country in countries" :key="country" :value="country">
-                            {{ country }}
+                        <option v-for="country in countries" :key="country.name" :value="country.value">
+                            {{ country.name }}
                         </option>
                     </select>
                 </div>
                 <div class="form-group">
-                    <label for="city">Şehir:</label>
+                    <label for="city">{{languageEn.weather.city}}:</label>
                     <select id="city" v-model="selectedCity">
-                        <option v-for="city in cities" :key="city" :value="city">
-                            {{ city }}
+                        <option v-for="city in cities" :key="city.name" :value="city.value">
+                            {{ city.name }}
                         </option>
                     </select>
                 </div>
-                <button type="submit">Hava Durumunu Getir</button>
+                <button type="submit">{{languageEn.button}}</button>
             </form>
         </div>
 
-        <div class="weather-table-container" v-if="weatherData">
-            <h2>{{ selectedCity }}, {{ selectedCountry }} Hava Durumu</h2>
+        <div class="weather-table-container" v-if="!validation(weatherResponseDto.city) && !validation(weatherResponseDto.country) && !loadingTable">
             <table class="weather-table">
                 <thead>
                 <tr>
-                    <th>Şehir</th>
-                    <th>Sıcaklık (°C)</th>
-                    <th>Nem (%)</th>
-                    <th>Rüzgar Hızı (km/h)</th>
-                    <th>Durum</th>
+                    <th>{{languageEn.currentTime}}</th>
+                    <th>{{languageEn.weather.time}}</th>
+                    <th>{{languageEn.weather.country}}</th>
+                    <th>{{languageEn.weather.city}}:</th>
+                    <th>{{languageEn.weather.temperature}}</th>
+                    <th>{{languageEn.weather.humidity}} </th>
+                    <th>{{languageEn.weather.wind}}</th>
+                    <th>{{ languageEn.weather.description }}</th>
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>{{ weatherData.city }}</td>
-                    <td>{{ weatherData.temperature }}</td>
-                    <td>{{ weatherData.humidity }}</td>
-                    <td>{{ weatherData.windSpeed }}</td>
-                    <td>{{ weatherData.condition }}</td>
+                <tr v-for="(data, index) in previousInfo" :key="index">
+                    <td>{{ data.localDateTime}}</td>
+                    <td>{{convertTime(data.dt)}}</td>
+                    <td>{{data.country}}</td>
+                    <td>{{ data.city }}</td>
+                    <td>{{ data.temp }}</td>
+                    <td>{{ data.humidity }}</td>
+                    <td>{{ data.windSpeed }}</td>
+                    <td>{{data.description }}</td>
                 </tr>
                 </tbody>
             </table>
@@ -51,88 +56,139 @@
     </div>
 </template>
 <script>
+import axios from "axios";
+import { DateTime } from 'luxon';
+
 export default {
     name: 'WeatherApp',
     data() {
         return {
-            countries: ['Türkiye', 'ABD', 'Almanya'],
+            countries: [
+                {name: "Default (Turkey)", value: null},
+                {name: "United States", value: "United States"},
+                {name: "Germany", value: "Germany"}
+            ],
             cities: [],
-            selectedCountry: '',
-            selectedCity: '',
-            weatherData: {
-                weatherList: {
-                    id: '',
-                    main: '',
-                    description: '',
-                    icon: ''
-                },
-                main: {
-                    temp: '',
-                    feels_like: '',
-                    pressure: '',
-                    humidity: ''
-                },
-                sys: {
-                    country:''
-                },
-                wind: {
-                    speed: '',
-                },
-                timezone: '',
-                name: '',
+            selectedCountry: null,
+            selectedCity: null,
+            weatherResponseDto: {
+                temp:'',
+                humidity: '',
+                pressure: '',
+                country: '',
+                description: '',
+                city: '',
+                windSpeed: '',
                 dt: '',
+                localDateTime: '',
             },
-            languageTr: {
-                header: 'Canlı Hava Durumu',
-                button: 'Hava Durumunu Getir',
+            languageEn: {
+                header: 'Live Weather',
+                button: 'Get Weather',
+                currentTime: "Current Time",
                 weather: {
-                    temperature: 'Sıcaklık(C)',
-                    wind: 'Rüzgar(km/h)',
-                    state: 'Durum',
-                    City: 'Şehir',
-                    Country: 'Ülke',
-                    Time: 'Zaman',
-                    feelsLike: 'Hissedilen',
+                    temperature: 'Temperature (°C)',
+                    wind: 'Wind (km/h)',
+                    state: 'Condition',
+                    humidity: "Humidity (%)",
+                    city: 'City',
+                    country: 'Country',
+                    time: 'Latest Live Data Update',
+                    feelsLike: 'Feels Like',
+                    description: "Situation",
                 }
-            }
+            },
+            loadingTable: false,
+            previousInfo: [],
+
 
         };
     },
     methods: {
-        fetchCities() {
-            if (this.selectedCountry === 'Türkiye') {
-                this.cities = ['Ankara', 'İstanbul', 'İzmir'];
-            } else if (this.selectedCountry === 'ABD') {
-                this.cities = ['New York', 'Los Angeles', 'Chicago'];
-            } else if (this.selectedCountry === 'Almanya') {
-                this.cities = ['Berlin', 'Münih', 'Hamburg'];
-            }
-            this.selectedCity = '';
+        convertTime(unixTimestamp){
+            const date = new Date(unixTimestamp * 1000);
+            return date.toLocaleString();
         },
-        fetchWeatherData() {
-            // Hava durumu verilerini API'dan çek
-            this.weatherData = {
-                city: this.selectedCity,
-                temperature: 25,
-                humidity: 60,
-                windSpeed: 15,
-                condition: 'Güneşli'
-            };
-        }
-    }
-};
+        validation(obj){
+            return obj === '' || obj === null || obj === undefined;
+        },
+        fetchCities() {
+            if(this.selectedCountry === null){
+                this.cities = [
+                    {name: "Default (Ankara)", value: null},
+                    {name: "İstanbul", value: "İstanbul"},
+                    {name: "İzmir", value: "İzmir"}
+                ];
+            }
+            if (this.selectedCountry === "Turkey") {
+                this.cities = [
+                    {name: "Default (Ankara)", value: null},
+                    {name: "İstanbul", value: "İstanbul"},
+                    {name: "İzmir", value: "İzmir"}
+                ];
+            } else if (this.selectedCountry === 'United States') {
+                this.cities = [
+                    {name: "New York", value: "New York"},
+                    {name: "Los Angeles", value: "Los Angeles"},
+                    {name: "Chicago", value: "Chicago"}
+                ];
+            } else if (this.selectedCountry === 'Germany') {
+                this.cities = [
+                    {name: "Berlin", value: "Berlin"},
+                    {name: "Munich", value: "Munich"},
+                    {name: "Hamburg", value: "Hamburg"}
+                ];
+            }
+            else {
+                this.selectedCity = null;
+            }
+        },
+         fetchWeatherData() {
+            this.loadingTable = true;
+             axios
+                .get("http://localhost:5005/weather/information", {
+                    params: {
+                        city: this.selectedCity,
+                        country: this.selectedCountry
+                    }
+                })
+                .then(response => {
+                    this.weatherResponseDto = response.data;
+                    this.previousInfo.unshift(this.weatherResponseDto)
+                    if (this.previousInfo.length > 6) {
+                        this.previousInfo.splice(6);
+                    }
+                    const originalDate = DateTime.local().toFormat('yyyy-MM-dd HH:mm:ss');
+                    const [date, time] = originalDate.split(' ');
+                    const [year, month, day] = date.split('-');
+                    this.weatherResponseDto.localDateTime = `${day}.${month}.${year} ${time}`;
+                })
+                .catch(error => {
+                    console.error("Weather data fetch error:", error);
+                });
+            this.loadingTable = false;
+
+
+        },
+    },
+    mounted(){
+        this.fetchCities();
+        this.fetchWeatherData();
+    },
+}
+
 </script>
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
 
 .weather-app {
     width: 100%;
-    height: 100vh;
+    height: 98vh;
     display: flex;
     flex-direction: column;
     align-items: center;
     font-family: 'Roboto', sans-serif;
-    overflow: hidden;
+    overflow-y: hidden;
     position: relative;
 }
 
@@ -152,7 +208,7 @@ export default {
 .header {
     width: 100%;
     max-width: 1200px;
-    padding: 40px;
+    padding: 20px;
     background: #007bff;
     color: #fff;
     text-align: center;
@@ -231,17 +287,20 @@ button:hover {
 
 .weather-table-container {
     width: 100%;
-    max-width: 800px;
+    max-width: 1230px;
     background: #fff;
     padding: 20px;
     border-radius: 15px;
     box-shadow: 0 6px 15px rgba(0, 0, 0, 0.2);
-    margin-top: 20px;
+    margin-top: 1px;
+    max-height: 500px; /* Bu değeri ihtiyacınıza göre ayarlayın */
+    overflow-y: auto; /* Dikey kaydırma çubuğunu gösterir */
 }
+
 
 .weather-table-container h2 {
     font-size: 2em;
-    margin-bottom: 20px;
+    margin-bottom: 8px;
     color: #333;
     text-align: center;
     font-weight: 700;
